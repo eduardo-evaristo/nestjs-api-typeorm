c61139cda71b9c65a0655f75399419a22110d5a6
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-01-19 20:15:26 -0300

    feat(auth): add auth module with register method

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  singUp(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { CreateUserParams } from 'src/users/constants/createUserParams';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly usersService: UsersService) {}
+
+  async signUp(createUserDto: CreateUserDto) {
+    //Check if email is already registered
+    const user = await this.usersService.findByEmail(createUserDto.email);
+
+    //If user with said email exists, throw an error
+    if (user) throw new BadRequestException('Email is already registered');
+
+    //If not taken, check if provided passwords match
+    if (!(createUserDto.password === createUserDto.confirmPassword))
+      throw new BadRequestException('Passwords do not match');
+
+    //If passwords match, hash it - extract this into a helper fn
+    const password = createUserDto.password;
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //After password has been hashed, create a CreateUserParams obj
+    const toBeCreatedUser: CreateUserParams = {
+      displayName: createUserDto.displayName,
+      email: createUserDto.email,
+      password: hashedPassword,
+    };
+
+    //Pass it down to createUser
+    return this.usersService.createUser(toBeCreatedUser);
+  }
+}
